c9f87749f7ae9c6f8a286f15804d668be99a7749
Rifkimr <reefqe774@gmail.com>
2023-11-15 14:50:32 +0700

    setting api

--- fe/src/libs/api.ts
+++ fe/src/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "kriptostore-production.up.railway.app",
+  baseURL: "https://kriptostore-production.up.railway.app/",
 });
 
 export function setAuthToken(token: string) {
