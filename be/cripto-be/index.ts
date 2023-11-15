41ae13cac23e609a6dea913cba7e8da731f8324f
Rifkimr <reefqe774@gmail.com>
2023-11-15 15:20:50 +0700

    seeting data sourch

--- be/cripto-be/index.ts
+++ be/cripto-be/index.ts
@@ -1,6 +1,5 @@
 import * as express from "express";
 import { Request, Response } from "express";
-import * as cors from "cors";
 import { AppDataSource } from "./src/data-source";
 import router from "./src/route/index";
 import dotenv = require("dotenv");
@@ -11,6 +10,7 @@ AppDataSource.initialize()
   .then(async () => {
     const app = express();
     const port = process.env.PORT;
+    const cors = require("cors");
 
     // const router = express.Router();
     app.use(cors());
